ca5e78fca84d661837c708467302d0b1742ea93a
Daniele <daniele.capa@gmail.com>
2024-04-22 17:29:59 +0200

    Fixed everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Form from './components/Form'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
-
+      <Navbar />
+      <Hero />
       <Form /> 
     </>
   )
