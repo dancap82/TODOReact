fd6d0f95b6197db8b4e621a18d64675865f612f1
Daniele <daniele.capa@gmail.com>
2024-04-23 15:53:26 +0200

    removed useless import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
 import Form from './components/Form'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
