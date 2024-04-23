3b129154df134efb0a889686edb724cd9e28b231
roycejohn <cuevasroycejohn.com>
2024-04-23 16:43:28 +0200

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import TodoList from './components/Todolist.jsx';
-import Header from './components/Header.jsx';
-import Form from './components/Form.jsx';
 
 
 
@@ -11,20 +9,9 @@ import Form from './components/Form.jsx';
 function App() {
   return(
     <div>
-      <Header />
-      <div className="container">
-        <h2 className="text-center my-4">MY SCHEDULE</h2>
-      </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <Form />
-            </div>
-            <div className="col-md-6">
+
               <TodoList />
-            </div>
-          </div>
-        </div>
+    
     </div>
   );
 
